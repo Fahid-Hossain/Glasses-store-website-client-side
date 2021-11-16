b319481bf838d0b224d31e96dfe4832462820ce7
Fahid Hossain <fahidhossain2021@gmail.com>
2021-11-17 02:38:16 +0600

    make admin an user

--- src/pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,24 +1,45 @@
-import { Button, TextField } from '@mui/material';
+import { Alert, Button, TextField } from '@mui/material';
 import React, { useState } from 'react';
 
 const MakeAdmin = () => {
-    const [email,setEmail]= useState("");
+    const [email, setEmail] = useState("");
+    const [success,setSuccess] = useState(false);
 
-    const handleOnBlur = (e)=>{
+    const handleOnBlur = (e) => {
         setEmail(e.target.value);
     }
 
-    const handleAdminSubmit =(e)=>{
+    const handleAdminSubmit = (e) => {
+        const user = { email };
         e.preventDefault();
+        fetch("http://localhost:5000/users/admin", {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                if(data.modifiedCount){
+                    setEmail("");
+                    setSuccess(true);
+                }
+            })
 
     }
     return (
-        <div style={{marginTop:"5rem"}}>
-            <h2 style={{color:"green"}}>Make An Admin</h2>
+        <div style={{ marginTop: "5rem" }}>
+            <h2 style={{ color: "green" }}>Make An Admin</h2>
+            {
+                success && <Alert severity="success" style={{ width: "50%", margin: "18px auto" }}>Made Admin successfully !</Alert>
+            }
             <form onSubmit={handleAdminSubmit}>
-            <TextField style={{width: '30rem'}} id="standard-basic" label="Email" variant="standard" type="email" onBlur={handleOnBlur}/>
+                <TextField style={{ width: '30rem' }} id="standard-basic" label="Email" variant="standard" type="email" onBlur={handleOnBlur} />
+                <br />
+                <Button style={{ marginTop: "15px" }} variant="outlined" type="submit">Submit</Button>
             </form>
-            <Button style={{marginTop:"15px"}} variant="outlined">Submit</Button>
         </div>
     );
 };
